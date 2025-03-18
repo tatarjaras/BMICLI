aeb38b51acaa46e99d061161e06c96f7f4be4825
tatarjaras <havasim@kkszki.hu>
2025-03-18 12:03:30 +0100

    Add project files.

---
+++ BMICLI/Diak.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMICLI
+{
+    public class Diak
+    {
+        public string? Nev { get; set; }
+        public int Eletkor { get; set; }
+        public int Magassag { get; set; }
+        public int Testsuly { get; set; }
+
+        public Diak(string sor) 
+        {
+            string[] bontas = sor.Split(';');
+            Nev = bontas[0];
+            Eletkor = int.Parse(bontas[1]);
+            Magassag = int.Parse(bontas[2]);
+            Testsuly = int.Parse(bontas[3]);
+        }
+
+    }
+}
---
+++ BMICLI/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace BMICLI
+{
+    internal class Program
+    {
+        static List<Diak> diakok = new List<Diak>();
+        static void Main(string[] args)
+        {
+            string filePath = "bmi.txt";
+            string[] sorok = File.ReadAllLines(filePath);
+            for (int i = 0; i < sorok.Length; i++)
+            {
+                diakok.Add(new Diak(sorok[i]));
+            }
+
+            Console.WriteLine($"3. a, feladat: A diákok száma:{diakok.Count}");
+            Diak legmagasabb = diakok[0];
+            foreach (var item in diakok)
+            {
+                if (legmagasabb.Magassag > item.Magassag) 
+                {
+                    legmagasabb = item;
+                }
+                
+                Console.WriteLine(item.Magassag);
+
+            }
+        }
+    }
+}
